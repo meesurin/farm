c38d5260190c32aabf73f7206bd532f3406b6f53
meesurin <57745544+meesurin@users.noreply.github.com>
2019-11-14 18:50:07 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,8 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'GfXsrQMKtT6R9Ec7mWiosbDdXjbnqGcBN5FtSqMdUVW2KjE+Mp5RW9R7ItaTw8yOzp5NW9WnfM4UC2L63P16xErf2Ef0gwjBxcafyMbXyFtxEmlMOVk7Ii27AY1CibF2iAky6R7HzGgl5HKfaRT/cgdB04t89/1O/w1cDnyilFU=
+'; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +15,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => '1b97805593736694707fe22bfca74bfe',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
